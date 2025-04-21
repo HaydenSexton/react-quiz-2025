86d1151afe12d90a59fc7b6a97d8df169e0ec717
HaydenSexton <Hayden_Sexton@insideranken.org>
2025-04-21 00:08:45 -0500

    commit fixes

---
+++ react-quiz-app/src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <header style={{ textAlign: 'center'}}>
+      <h1>Vite and React Quiz Questions</h1>
+    </header>
+  );
+}
+
+export default Header;
